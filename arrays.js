73e81fdf383f344ef1d163ac353d2ba88b2b4641
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-19 20:54:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,17 @@
 var chocolateBars=["snickers", "hundred grand", "kitkat","skittles"]
+
+function addElementToBeginningOfArray(givenArray, value){
+  givenArray.unshift(value)
+   var newArray = [value, ...givenArray]
+  return newArray
+}
+
+function destructivelyAddElementToBEginningOfArray(givenArray, value){
+  
+  
+  var newArray = [value, ...givenArray]
+  
+  return newArray
+}
+
+return
